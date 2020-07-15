c9079deaf5c408437bbd746fa3a3aeebe296553a
Quang Nguyen <quangpmnguyen@gmail.com>
2020-07-15 14:56:25 -0400

    New interactive plots

--- 01_raw_data/server.R
+++ 01_raw_data/server.R
@@ -5,138 +5,134 @@ library(readr)
 library(forecast)
 library(plotly)
 library(corrr)
-library(zoo)
+library(glue)
 
 
-function(input, output,session){
-  # Getting the data in 
+function(input, output, session){
+  # Reactive input files for dates 
+  summer <- list("June 5th 2018" = "June_5_2018", "June 6th 2018" = "June_6_2018")
+  winter <- list("Jan 19th 2018" = "Jan_19_2018", "Jan 20th 2018" = "Jan_20_2018")
   
+  # List of variables and their more formatted names for easy plotting
+  variables <- list("Vertical Wind Speed" = "w" , 
+                  "Horizontal Wind Speed (North)" = "v" , 
+                  "Horizontal Wind Speed (East)" ="u",
+                  "CO2" = "CO2",
+                  "Water Vapor" = "H2O",
+                  "Air Temperature" = "airtemp")
+  
+  # observe the input$season to see which dates to see
   observe({
     if(grepl(input$season, "winter")){
-      updateSelectInput(session, inputId = "file",
-                        choices = list("jan 19 2018" =  "jan_19_2018", "jan 20 2018"="jan_20_2018"), 
-                        selected = "jan_19_2018")
+      updateSelectInput(session, inputId = "date",
+                        choices = winter, 
+                        selected = winter[[1]])
     }
     else{
-      updateSelectInput(session, inputId = "file",
-                        choices = list("june 5 2018" =  "june_5_2018", "june 6 2018"="june_6_2018"), 
-                        selected = "june_5_2018")
+      updateSelectInput(session, inputId = "date",
+                        choices = summer, 
+                        selected = summer[[1]])
     }
   })
   
   path <- reactive({
-    req(input$file)
-    paste("../data/processed_data/silas_little_", 
-          input$season, "_", input$file, ".rds", sep="")
+    req(input$date)
+    glue("../data/processed_data/{site}_{season}_{date}.rds", site = input$site, 
+         season = input$season, date = input$date)
   })
   
- # output$checking <-renderText({
-  #  path()
-   # })
-  data <- reactive({
-    as_tibble(rollmean(readRDS(path()),as.numeric(as.character(input$window_size)), align = "center"))
-  });
-   
+  # load data on push load_data
+  data <- eventReactive(eventExpr = input$load,{
+      readRDS(path())
+  })
   
   # Getting head table 
+  # TODO: Control number of lines 
   output$preview <- renderTable({
-    head(data())
+    head(data(), 15)
   })
-  
-  
-  # Getting histogram
-  output$histogram <- renderPlotly({
-    req(data())
-    switch(input$histogram_var,
-           "CO2" = plot_ly(alpha = 0.6, nbinsx = input$histogram_bins) %>% 
-             add_histogram(data()$CO2, name = "CO2", histnorm = "percent")%>% 
-             layout(title = "Histogram of CO2", xaxis = list(title = "CO2"),yaxis = list(title = "percent")),
-           
-           "u" = plot_ly(alpha = 0.6, nbinsx = input$histogram_bins) %>% 
-             add_histogram(data()$u, name = "horizontal speed", histnorm = "percent")%>%
-             layout(title = "Histogram of horizontal speed", xaxis = list(title = "U"),
-                    yaxis = list(title = "percent")),
-           
-           "v" = plot_ly(alpha = 0.6, nbinsx = input$histogram_bins) %>% 
-             add_histogram(data()$v, name = "horizontal speed", histnorm = "percent")%>% 
-             layout(title = "Histogram of horizontal speed", xaxis = list(title = "V"),
-                    yaxis = list(title = "percent")),
-           
-           "w" = plot_ly(alpha = 0.6, nbinsx = input$histogram_bins) %>% 
-             add_histogram(data()$w, name = "vertical speed", histnorm = "percent")%>% 
-             layout(title = "Histogram of vertical speed", xaxis = list(title = "vertical speed"),
-                    yaxis = list(title = "percent")),
-           
-           "airtemp" = plot_ly(alpha = 0.6, nbinsx = input$histogram_bins) %>% 
-             add_histogram(data()$airtemp, name = "Temperature", histnorm = "percent")%>% 
-             layout(title = "Histogram of air Temperature", xaxis = list(title = "airTemp"),
-                    yaxis = list(title = "percent")),
-           
-           "H2O" = plot_ly(alpha = 0.6, nbinsx = input$histogram_bins) %>% 
-             add_histogram(data()$H2O, name = "Water", histnorm = "percent")%>% 
-             layout(title = "Histogram of H2O", xaxis = list(title = "H2O"),yaxis = list(title = "percent")),
-    )
-  })
-  
 
-  t <- reactiveValues(data = NULL)
-  unit <- reactiveValues(data = NULL);
+  #data <- reactive({
+  #  as_tibble(rollmean(readRDS(path()),as.numeric(as.character(input$window_size)), align = "center"))
+  #});
   
-  observeEvent(input$sec, {
-    t$data <- seq(0,86400, length = length(data()$CO2))
-    unit$data <- "seconds"
+  # Histogram plots  
+  hist_plt <- eventReactive(input$load_hist,{
+    req(data())
+    form_label <- names(variables)[which(variables == input$histogram_var)] # formatted labels
+    plt <- plot_ly(alpha = 0.6, nbinsx = input$histogram_bins) %>%
+      add_histogram(pull(data(),input$histogram_var), name = form_label, histnorm = "percent") %>%
+      layout(title = glue("Histogram of {var}", var = form_label), 
+             xaxis = list(title = form_label),
+             yaxis = list(title = "Percent"), bargap = 0.1)
+    plt
   })
   
-  observeEvent(input$min, {
-    t$data <- seq(0,86400, length = length(data()$CO2))/60
-    unit$data <- "minutes"
+  output$histogram <- renderPlotly({
+    hist_plt()
   })
   
-  observeEvent(input$hour, {
-    t$data <- seq(0,86400, length = length(data()$CO2))/3600
-    unit$data <- "hours"
-  })  
+  # Time plots 
+  #TODO: move form_label into reactive element 
+  skeleton_plt <- reactive({
+    form_label <- names(variables)[which(variables == input$vvt)]
+    plt <- plot_ly(type = "scatter", mode = "lines") %>% 
+      layout(title = glue("{var} vs Time", var = form_label, yaxis = list(title = form_label)))
+    plt
+  })
+  
+  # Control what to plot depending on what buttons were pushed 
+  axes_timeplt <- reactiveValues(x = NULL,y = NULL,xlab = NULL, ylab = NULL) 
+  # Observe the action buttons 
+  observeEvent(input$sec,{
+   form_label <- names(variables)[which(variables == input$vvt)]
+   axes_timeplt$x <- data()$second
+   axes_timeplt$y <- pull(data(), input$vvt)
+   axes_timeplt$xlab <- glue("Time ({unit})", unit = "seconds")
+   axes_timeplt$ylab <- form_label
+  })
+  observeEvent(input$min,{
+    form_label <- names(variables)[which(variables == input$vvt)]
+    axes_timeplt$x <- data()$minute
+    axes_timeplt$y <- forecast::ma(pull(data(), input$vvt),60)
+    axes_timeplt$xlab <- glue("Time ({unit})", unit = "minutes")
+    axes_timeplt$ylab <- glue("Moving Average {lab}", lab = form_label)
+  })
+  observeEvent(input$hour,{
+    form_label <- names(variables)[which(variables == input$vvt)]
+    axes_timeplt$x <- data()$hour
+    axes_timeplt$y <- forecast::ma(pull(data(), input$vvt),3600)
+    axes_timeplt$xlab <- glue("Time ({unit})", unit = "hours")
+    axes_timeplt$ylab <- glue("Moving Average {lab}", lab = form_label)
+  })
+  
   
   output$vvt_plt_vs_time <- renderPlotly({
-    
-    req(data())
-    if (is.null(t$data) || length(t$data) != length(data()$CO2)) return()
-    if (is.null(unit$data)) return()
-    switch(input$vvt,
-           "CO2" =  plot_ly(x = ~t$data,  y = ~data()$CO2, type = "scatter", mode = "lines") %>% 
-             layout(title = as.character(num_row), yaxis = list(title = "CO2"),xaxis = 
-                      list(title = paste("time(", unit$data,")", sep = ""))),
-           
-           "airtemp" = plot_ly(x = ~t$data,  y = ~data()$airtemp, type = "scatter", mode = "lines") %>% 
-             layout(title = "Temperature Vs Time", yaxis = list(title = "Temperature"),xaxis = 
-                      list(title = paste("time(", unit$data,")", sep = ""))),
-           
-           "H2O" = plot_ly(x = ~t$data,  y = ~data()$H2O, type = "scatter", mode = "lines") %>% 
-             layout(title = "H2O Vs Time", yaxis = list(title = "H2O"),xaxis = 
-                      list(title = paste("time(", unit$data,")", sep = ""))),
-           
-           "u" = plot_ly(x = ~t$data,  y = ~data()$u, type = "scatter", mode = "lines") %>% 
-             layout(title = "Horizontal Wind Speed (East) Vs Time", 
-                    yaxis = list(title = "Horizontal Wind Speed (East)"),xaxis = 
-                      list(title = paste("time(", unit$data,")", sep = ""))),
-           
-           "v" = plot_ly(x = ~t$data,  y = ~data()$v, type = "scatter", mode = "lines") %>% 
-             layout(title = "Horizontal Wind Speed (North) Vs Time", 
-                    yaxis = list(title = "Horizontal Wind Speed (North)"),xaxis = 
-                      list(title = paste("time(", unit$data,")", sep = ""))),
-           
-           "w" = plot_ly(x = ~t$data,  y = ~data()$w, type = "scatter", mode = "lines") %>% 
-             layout(title = "VerticalWind Speed Vs Time", 
-                    yaxis = list(title = "vertical Wind Speed"),xaxis = 
-                      list(title = paste("time(", unit$data,")", sep = ""))),
-    )
+    if(is.null(axes_timeplt$x) | is.null(axes_timeplt$y)) return()
+    form_label <- names(variables)[which(variables == input$vvt)]
+    skeleton_plt() %>% add_lines(x = axes_timeplt$x, y = axes_timeplt$y, name = form_label) %>%
+      layout(xaxis = list(title = axes_timeplt$xlab), yaxis = list(title = axes_timeplt$ylab))
   })
+
   # pair plot 
-  # TODO: Add functionality to 
-  plt <- reactive({ggplot(data(),
-                          aes(x = !!sym(input$pairplot_xvar), 
-                              y = !!sym(input$pairplot_yvar)))})
-  output$pairplot <- renderPlot({
-    plt() + geom_point() + geom_smooth()
+  
+  
+  pairplot <- eventReactive(input$load_pair,{
+    form_label_x <- names(variables)[which(variables == input$pairplot_xvar)]
+    form_label_y <- names(variables)[which(variables == input$pairplot_yvar)]
+    p <- plot_ly() %>% add_trace(x = pull(data(), input$pairplot_xvar), 
+                               y = pull(data(), input$pairplot_yvar), mode = "markers", type = "scattergl") %>%
+                        layout(xaxis = list(title = form_label_x), yaxis = list(title = form_label_y),
+                               title = glue("{xaxis} vs {yaxis}", xaxis = form_label_x, yaxis = form_label_y))
   })
+  
+  output$pairplot <- renderPlotly({
+    pairplot()
+  })
+  #plt <- reactive({ggplot(data(),
+  #                        aes(x = !!sym(input$pairplot_xvar), 
+  #                            y = !!sym(input$pairplot_yvar)))})
+  #output$pairplot <- renderPlot({
+  #  plt() + geom_point() + geom_smooth()
+  #})
 }
--- 01_raw_data/ui.R
+++ 01_raw_data/ui.R
@@ -1,34 +1,48 @@
 library(shiny)
 library(plotly)
 options(shiny.maxRequestSize=50*1024^2)
+
+# ui elements 
 fluidPage(
   h3("File selection"),
-  inputPanel(selectInput("season", h3("Select season"),
-                         choices = list("summer", "winter"), selected = 1)),
-  inputPanel(
-    #TODO: https://stackoverflow.com/questions/21465411/r-shiny-passing-reactive-to-selectinput-choices
-    selectInput("file", h3("Select date"), 
-                choices = list("june 5 2018" =  "june_5_2018", "june 6 2018"="june_6_2018"), 
-                selected = "june_5_2018")
+  sidebarLayout(
+    sidebarPanel(
+      selectInput("site", h3("Select Site"), choices = list("Silas Little" = "silas_little"), 
+                  selected = "silas_little"),
+      selectInput("season", h3("Select season"),
+                  choices = list("summer", "winter"), selected = 1),
+      selectInput("date", h3("Select date"), ""),
+      actionButton(inputId = "load", "Load Data")
+    ),
+    mainPanel(
+      h4("Data Preview"),
+      tableOutput(outputId = "preview")
+    )
   ),
-  h3("Data Preview"),
-  selectInput("window_size", "Select the size of window for running average", 
-              choices = list("10 seconds" = "10", "30 seconds " = "30" , "1 minute" = "60"),
-              selected = "10 seconds"),
-  textOutput("checking"),
   
-  tableOutput("preview"),
+  
+  #h3("Data Preview"),
+  #selectInput("window_size", "Select the size of window for running average", 
+  #            choices = list("10 seconds" = "10", "30 seconds " = "30" , "1 minute" = "60"),
+  #            selected = "10 seconds"),
+  #textOutput("checking"),
+  
   # Histogram
   h3("Histogram"),
   sidebarLayout(
     sidebarPanel(
       selectInput("histogram_var", h4("Select box"), 
-                  choices = list("CO2" = "CO2", "horizontal speed 1" = "u" , "horizontal speed 1" = "v" , 
-                                 "vertical Speed" = "w", "Temperature" = "airtemp", "H2O" = "H2O"),
+                  choices = list( "Vertical Wind Speed" = "w" , 
+                                  "Horizontal Wind Speed (North)" = "v" , 
+                                  "Horizontal Wind Speed (East)" ="u",
+                                  "CO2" = "CO2",
+                                  "Water Vapor" = "H2O",
+                                  "Air Temperature" = "airtemp"),
                   selected = "CO2"),
       
       sliderInput("histogram_bins", h4("number of bins"),
-                  min = 0, max = 100, value = 50)
+                  min = 0, max = 100, value = 50),
+      actionButton("load_hist", "Load Graph")
     ),
     mainPanel(
       plotlyOutput("histogram")
@@ -36,7 +50,7 @@ fluidPage(
   ),
   
   # Variable vs time  
-  h3("variable vs Time"),
+  h3("Variable vs Time"),
   sidebarLayout(
     sidebarPanel(
       selectInput("vvt", "Select Variables", 
@@ -61,18 +75,26 @@ fluidPage(
   h3("Bivariate plots"),
   sidebarLayout(
     sidebarPanel(
-      selectInput('pairplot_xvar', "X Axis", 
-                  choices = list("CO2"="CO2","H2O"="H2O","Temperature"='airtemp',
-                                "u" = "u", "w" = 'w', "v"="v")),
-      selectInput('pairplot_yvar', "Y Axis", 
-                  choices = list("CO2"="CO2","H2O"="H2O","Temperature"="airtemp",
-                                 "u" = "u", "w" = 'w', "v"="v"), selected = "airtemp"),
-      actionButton("pairplot_second", "Second"),
-      actionButton("pairplot_minute", "Minute"),
-      actionButton("pairplot_hour", "Hour")
+      selectInput("pairplot_xvar", "Select X Axis", 
+                  choices = list( "Vertical Wind Speed" = "w" , 
+                                  "Horizontal Wind Speed (North)" = "v" , 
+                                  "Horizontal Wind Speed (East)" ="u",
+                                  "CO2" = "CO2",
+                                  "Water Vapor" = "H2O",
+                                  "Air Temperature" = "airtemp"),
+                  selected = "w"),
+      selectInput("pairplot_yvar", "Select Y Axis", 
+                  choices = list( "Vertical Wind Speed" = "w" , 
+                                  "Horizontal Wind Speed (North)" = "v" , 
+                                  "Horizontal Wind Speed (East)" ="u",
+                                  "CO2" = "CO2",
+                                  "Water Vapor" = "H2O",
+                                  "Air Temperature" = "airtemp"),
+                  selected = "v"),
+      actionButton("load_pair", "Load Graph")
     ),
     mainPanel(
-      plotOutput("pairplot")
+      plotlyOutput("pairplot")
     )
   )
 )
