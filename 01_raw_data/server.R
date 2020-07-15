4c2160f78b279fc59dd20bbd1fedb1c41bed78ab
Pierre Breau <benzy00@Pierres-MacBook-Pro-2.local>
2020-07-15 05:30:27 -0400

    integrated plotly

--- 01_raw_data/server.R
+++ 01_raw_data/server.R
@@ -3,13 +3,38 @@ library(tidyverse)
 library(rlang)
 library(readr)
 library(corrr)
+library(plotly)
+library(zoo)
 
-function(input, output){
+function(input, output,session){
   # Getting the data in 
-  data <- reactive({
-    req(input$rawdata)
-    as_tibble(read_csv(input$rawdata$datapath, n_max = input$n_row))
+  
+  observe({
+    if(grepl(input$season, "winter")){
+      updateSelectInput(session, inputId = "file",
+                        choices = list("jan 19 2018" =  "jan_19_2018", "jan 20 2018"="jan_20_2018"), 
+                        selected = "jan_19_2018")
+    }
+    else{
+      updateSelectInput(session, inputId = "file",
+                        choices = list("june 5 2018" =  "june_5_2018", "june 6 2018"="june_6_2018"), 
+                        selected = "june_5_2018")
+    }
   })
+  
+  path <- reactive({
+    req(input$file)
+    paste("../data/processed_data/silas_little_", 
+          input$season, "_", input$file, ".rds", sep="")
+  })
+  
+ # output$checking <-renderText({
+  #  path()
+   # })
+  data <- reactive({
+    as_tibble(rollmean(readRDS(path()),as.numeric(as.character(input$window_size)), align = "center"))
+  });
+   
   
   # Getting head table 
   output$preview <- renderTable({
@@ -18,37 +43,103 @@ function(input, output){
   
   
   # Getting histogram
-  output$histogram <- renderPlot({
-    hist(data()$CO2)
+  output$histogram <- renderPlotly({
+    req(data())
     switch(input$histogram_var,
-           "CO2" = hist(data()$CO2, breaks = input$histogram_bins + 1,col = "#75AADB", border = "white"),
-           "u" = hist(data()$u, breaks = input$histogram_bins + 1,col = "#75AADB", border = "white"),
-           "v" = hist(data()$v, breaks = input$histogram_bins + 1,col = "#75AADB", border = "white"),
-           "w" = hist(data()$w, breaks = input$histogram_bins + 1,col = "#75AADB", border = "white"),
-           "airtemp" = hist(data()$airtemp, breaks = input$histogram_bins + 1,col = "#75AADB", border = "white"))
+           "CO2" = plot_ly(alpha = 0.6, nbinsx = input$histogram_bins) %>% 
+             add_histogram(data()$CO2, name = "CO2", histnorm = "percent")%>% 
+             layout(title = "Histogram of CO2", xaxis = list(title = "CO2"),yaxis = list(title = "percent")),
+           
+           "u" = plot_ly(alpha = 0.6, nbinsx = input$histogram_bins) %>% 
+             add_histogram(data()$u, name = "horizontal speed", histnorm = "percent")%>%
+             layout(title = "Histogram of horizontal speed", xaxis = list(title = "U"),
+                    yaxis = list(title = "percent")),
+           
+           "v" = plot_ly(alpha = 0.6, nbinsx = input$histogram_bins) %>% 
+             add_histogram(data()$v, name = "horizontal speed", histnorm = "percent")%>% 
+             layout(title = "Histogram of horizontal speed", xaxis = list(title = "V"),
+                    yaxis = list(title = "percent")),
+           
+           "w" = plot_ly(alpha = 0.6, nbinsx = input$histogram_bins) %>% 
+             add_histogram(data()$w, name = "vertical speed", histnorm = "percent")%>% 
+             layout(title = "Histogram of vertical speed", xaxis = list(title = "vertical speed"),
+                    yaxis = list(title = "percent")),
+           
+           "airtemp" = plot_ly(alpha = 0.6, nbinsx = input$histogram_bins) %>% 
+             add_histogram(data()$airtemp, name = "Temperature", histnorm = "percent")%>% 
+             layout(title = "Histogram of air Temperature", xaxis = list(title = "airTemp"),
+                    yaxis = list(title = "percent")),
+           
+           "H2O" = plot_ly(alpha = 0.6, nbinsx = input$histogram_bins) %>% 
+             add_histogram(data()$H2O, name = "Water", histnorm = "percent")%>% 
+             layout(title = "Histogram of H2O", xaxis = list(title = "H2O"),yaxis = list(title = "percent")),
+    )
   })
 
-  output$vvt_plt <- renderPlot({
+  output$vvt_plt <- renderPlotly({
+    req(data())
       col_val=c()
       var_data = c()
       col_list=c("blue", "red","black")
       for(i in 1:length(input$vvt_wind)){
-        col_val = c(col_val, col_list[grep(input$vvt_wind[i], names(data()))])
-        var_data = c(var_data, data()[grep(input$vvt_wind[i], names(data()))])
+        col_val = c(col_val, col_list[grep(paste("^", input$vvt_wind[i],"$", sep = ""), names(data()))])
+        var_data = c(var_data, data()[grep(paste("^", input$vvt_wind[i],"$", sep = ""), names(data()))])
       }
-      
-      t<-seq(0,input$n_row, length = input$n_row)
-      var_data[[1]] = var_data[[1]][!is.na(var_data[[1]])]
-      plot(t,  var_data[[1]], xlab="time(hours)", ylab = "wind", type="s",
-           col=col_val[1])
-      for(i in 1:length(var_data)){
-        var_data[[i]] = var_data[[i]][!is.na(var_data[[i]])]
-        lines(t,var_data[[i]], col=col_val[i])
+      num_row = length(data()$u)
+      time_divisor <- 1;
+      if(grepl("min", input$vvt_time)){
+        time_divisor <- 60;
       }
+      else if(grepl("hours", input$vvt_time)){
+        time_divisor <- 3600;
+      }
+      t<-seq(0,86400, length = num_row)/time_divisor
       
-      legend("bottomright", input$vvt_wind, col=col_val, bg= "gray93")
+      if(length(var_data) > 0){
+        var_data[[1]] = var_data[[1]][!is.na(var_data[[1]])]
+        fig <- plot_ly(x = ~t,  y = ~var_data[[1]], type = "scatter", mode = "lines",
+                       color = I(col_list[1]), name = input$vvt_wind[[1]])
+        for(i in min(2, length(var_data)):length(var_data)){
+          if(i > 1) {
+            var_data[[i]] = var_data[[i]][!is.na(var_data[[i]])]
+            fig <- fig %>% add_trace(x = ~t, y = ~var_data[[i]], type = "scatter", mode = "lines",
+                                   color = I(col_list[i]), name = input$vvt_wind[[i]])
+          }
+        
+        }
+        fig <- fig %>% layout(title = "wind versus time",
+                xaxis = list(title = paste("time(", input$vvt_time,")", sep ="")),
+                yaxis = list(title = "Wind"))
+      }
   })
   
+  output$vvt_plt_vs_time <- renderPlotly({
+    req(data())
+    num_row = length(data()$CO2)
+    time_divisor <- 1;
+    if(grepl("min", input$v_time)){
+      time_divisor <- 60;
+    }
+    else if(grepl("hours", input$v_time)){
+      time_divisor <- 3600;
+    }
+    t<-seq(0,84400, length = num_row)/time_divisor
+   
+    
+    switch(input$vvt,
+           "CO2" =  plot_ly(x = ~t,  y = ~data()$CO2, type = "scatter", mode = "lines") %>% 
+             layout(title = "CO2 Vs Time", yaxis = list(title = "CO2"),xaxis = 
+                      list(title = paste("time(", input$v_time,")", sep = ""))),
+           
+           "airtemp" = plot_ly(x = ~t,  y = ~data()$airtemp, type = "scatter", mode = "lines") %>% 
+             layout(title = "Temperature Vs Time", yaxis = list(title = "Temperature"),xaxis = 
+                      list(title = paste("time(", input$v_time,")", sep = ""))),
+           
+           "H2O" = plot_ly(x = ~t,  y = ~data()$H2O, type = "scatter", mode = "lines") %>% 
+             layout(title = "H2O Vs Time", yaxis = list(title = "H2O"),xaxis = 
+                      list(title = paste("time(", input$v_time,")", sep = ""))),
+    )
+  })
   # pair plot 
   # TODO: Add functionality to 
   plt <- reactive({ggplot(data(),
--- 01_raw_data/ui.R
+++ 01_raw_data/ui.R
@@ -1,63 +1,67 @@
 library(shiny)
+library(plotly)
 options(shiny.maxRequestSize=50*1024^2)
 fluidPage(
+  h3("File selection"),
+  inputPanel(selectInput("season", h3("Select season"),
+                         choices = list("summer", "winter"), selected = 1)),
+  inputPanel(
+    selectInput("file", h3("Select date"), 
+                choices = list("june 5 2018" =  "june_5_2018", "june 6 2018"="june_6_2018"), 
+                selected = "june_5_2018")
+  ),
   h3("Data Preview"),
-  sidebarLayout(
-    sidebarPanel(
-      fileInput("rawdata", "Choose CSV File", multiple = T, 
-                accept = c("text/csv","text/comma-separated-values,text/plain",".csv")),
-      numericInput("n_row", "Number of rows to import", value = 6000, max = 864000,
-                     step = 10)
-    ),
-    mainPanel(
-      tableOutput("preview")
-    )
-  ),
-  h3("Alternative file selection process"),
-  inputPanel(selectInput("season", h3("Select season"), 
-                         choices = list("Summer", "Winter"), selected = "Summer")),
-  conditionalPanel(
-    condition = "input.season === Summer",
-    selectInput("file", h3("Select date"), 
-                choices = list("june 5 2018", "june 6 2018"), 
-                selected = "june 5 2018")
-  ),
-  conditionalPanel(
-    condition = "input.season === Winter",
-    selectInput("file", h3("Select date"), 
-                choices = list("jan 19 2018" , "jan 20 2018"), 
-                selected = "jan 19 2018")
-  ),
+  selectInput("window_size", "Select the size of window for running average", 
+              choices = list("10 seconds" = "10", "30 seconds " = "30" , "1 minute" = "60"),
+              selected = "10 seconds"),
+  textOutput("checking"),
+  
+  tableOutput("preview"),
   # Histogram
   h3("Histogram"),
   sidebarLayout(
     sidebarPanel(
       selectInput("histogram_var", h4("Select box"), 
-                  choices = list("CO2", "u" , "v" , "w",
-                                 "airtemp"),
-                  selected = 1),
+                  choices = list("CO2" = "CO2", "horizontal speed 1" = "u" , "horizontal speed 1" = "v" , 
+                                 "vertical Speed" = "w", "Temperature" = "airtemp", "H2O" = "H2O"),
+                  selected = "CO2"),
       
       sliderInput("histogram_bins", h4("number of bins"),
                   min = 0, max = 100, value = 50)
     ),
     mainPanel(
-      plotOutput("histogram")
+      plotlyOutput("histogram")
     )
   ),
   
   # Variable vs time  
-  h3("Variable vs Time"),
+  h3("wind vs Time"),
   sidebarLayout(
     sidebarPanel(
       checkboxGroupInput("vvt_wind", "Select Variables", 
                         choices = list( "vertical wind speed" = "u" , "horizontal wind speed1" = "v" , "horizontal wind speed2" ="w"),
                         selected = "u"),
       selectInput("vvt_time", "Select Time scale", 
-                  choices = list("1/10th sec" = 1, "sec" = 2, "min" = 3, "hours"= 4),
-                  selected = 1)
+                  choices = list("seconds" = "sec", "minutes" = "min" , "hours" = "hours"),
+                  selected = "sec")
     ),
     mainPanel(
-      plotOutput("vvt_plt")
+      plotlyOutput("vvt_plt")
+    )
+  ),
+  
+  h3("Variable vs Time"),
+  sidebarLayout(
+    sidebarPanel(
+      selectInput("vvt", "Select Variables", 
+                         choices = list( "CO2" = "CO2" , "Temperature" = "airtemp" , "H2O" ="H2O"),
+                         selected = "CO2"),
+      selectInput("v_time", "Select Time scale", 
+                  choices = list("seconds" = "sec", "minute" = "min" , "hours" = "hours"),
+                  selected = "sec")
+    ),
+    mainPanel(
+      plotlyOutput("vvt_plt_vs_time"),
     )
   ),
   
