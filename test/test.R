767b396b794bbb8b7ba479ec93d46c515868132b
Quang Nguyen <quangpmnguyen@gmail.com>
2020-10-30 10:25:33 -0400

    Removed all in test folder

--- test/test.R
+++
@@ -1,8 +0,0 @@
-library(tidyverse)
-library(plotly)
-
-number_of_charts <- seq(1,100)
-fun_factor <- number_of_charts^3
-plot_ly(x = number_of_charts, y = fun_factor) %>% layout(title = "Cubic Fit of # of charts vs fun factor",
-                                                         xaxis = list(title = "Number of Charts"), 
-                                                         yaxis = list(title = "Fun Factor"))
